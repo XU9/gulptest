adb788a54419106d9808b84302612712d652afc7
XYZ <NHAXYZ@163.com>
2017-05-16 17:50:37 +0800

    All plug-in has been installed, then add photos and writing HTML, less, js to test him

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+//导入工具包 require('node_modules里对应模块')
+var gulp = require('gulp'), //本地安装gulp所用到的地方
+    less = require('gulp-less'),
+    htmlmin = require('gulp-htmlmin'),
+    uglify= require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
+    browserSync = require('browser-sync').create(),
+	reload=browserSync.reload;
+ 
+//定义一个testLess任务（自定义任务名称）
+gulp.task('testLess', function () {
+	//gulp.src(['src/less/index.less','src/less/detail.less']) 
+	//多个文件以数组形式传入
+        gulp.src('src/less/index.less') //该任务针对的文件
+        .pipe(less()) //该任务调用的模块
+        .pipe(gulp.dest('src/css'))//将会在src/css下生成index.css
+        .pipe(reload({stream:true}));//相当于.pipe(browserSync.stream());该 stream 方法返回一个变换流，并且可以充当一次或多个文件。添加自动刷新时需要添加这一句
+});
+     
+gulp.task('testHtmlmin', function () {
+    var options = {
+        removeComments: true,//清除HTML注释
+        collapseWhitespace: true,//压缩HTML
+        collapseBooleanAttributes: true,//省略布尔属性的值 <input checked="true"/> ==> <input />
+        removeEmptyAttributes: true,//删除所有空格作属性值 <input id="" /> ==> <input />
+        removeScriptTypeAttributes: true,//删除<script>的type="text/javascript"
+        removeStyleLinkTypeAttributes: true,//删除<style>和<link>的type="text/css"
+        minifyJS: true,//压缩页面JS
+        minifyCSS: true//压缩页面CSS
+    };
+    gulp.src('src/html/*.html')
+        .pipe(htmlmin(options))
+        .pipe(gulp.dest('dist/html'))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('testjsmin', function () {
+	//压缩src/js目录下的所有js文件
+    //除了test1.js和test2.js（**匹配src/js的0个或多个子文件夹）
+    gulp.src(['src/js/*.js', '!src/js/**/{test1,test2}.js'])
+        .pipe(uglify({
+            mangle: true,//类型：Boolean 默认：true 是否修改变量名
+            compress: true,//类型：Boolean 默认：true 是否完全压缩
+            preserveComments: 'all' //保留所有注释
+        }))
+        .pipe(gulp.dest('dist/js'))
+        .pipe(reload({stream:true}));
+});
+ 
+gulp.task('testConcat', function () {
+    gulp.src('src/js/*.js')
+        .pipe(concat('all.js'))//合并后的文件名
+        .pipe(gulp.dest('dist/js'))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('testImagemin', function () {
+    gulp.src('src/img/*.{png,jpg,gif,ico}')
+        .pipe(imagemin({
+            optimizationLevel: 5, //类型：Number  默认：3  取值范围：0-7（优化等级）
+            progressive: true, //类型：Boolean 默认：false 无损压缩jpg图片
+            interlaced: true, //类型：Boolean 默认：false 隔行扫描gif进行渲染
+            multipass: true //类型：Boolean 默认：false 多次优化svg直到完全优化
+        }))
+        .pipe(gulp.dest('dist/img'))
+        .pipe(reload({stream:true}));
+});
+// 静态服务器
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        }
+    });
+     
+    gulp.watch('src/less/*.less',['testLess']);//监听当所有less文件发生改变时，调用testLess任务
+    gulp.watch('src/js/*.js',['testjsmin']);
+    gulp.watch('src/js/*.js',['testConcat']);
+    gulp.watch('src/imgs/*.{png,jpg,gif,ico}',['testImagemin']);
+    gulp.watch('src/html/*.html',['testHtmlmin']).on('change',reload);
+});
+
+// 代理
+
+// gulp.task('browser-sync', function() {
+//     browserSync.init({
+//         proxy: "你的域名或IP"
+//     });
+// });
+gulp.task('default',['testLess', 'testHtmlmin','testjsmin','testConcat','testImagemin','browser-sync']);
+//gulp.task('default',['testLess', 'elseTask']);定义默认任务 elseTask为其他任务，该示例没有定义elseTask任务
+ 
+//gulp.task(name[, deps], fn) 定义任务  name：任务名称 deps：依赖任务名称 fn：回调函数
+//gulp.src(globs[, options]) 执行任务处理的文件  globs：处理的文件路径(字符串或者字符串数组) 
+//gulp.dest(path[, options]) 处理完后文件生成路径
